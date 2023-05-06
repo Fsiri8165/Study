99d37acd0d5999071fd6dfa83534446f7faaca96
신용대 <@175.210.74.152>
2023-05-06 15:05:06 +0900

    변수명 정하기

--- Study0506/src/com/study/main/Chart.java
+++ Study0506/src/com/study/main/Chart.java
@@ -1,5 +1,12 @@
 package com.study.main;
 
 public interface Chart {
+//	String[][] table;
+//	int curRow;
+//	int curCol;
 	void insertCommand(String s);
+	void move(String s);
+	void delete();
+	void print();
+	void input(String s);
 }
