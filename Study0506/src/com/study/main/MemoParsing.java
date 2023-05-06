493c7597538257a003a869910f5aa00a3fe44ebe
82109 <82109@DESKTOP-3A7389K>
2023-05-06 17:25:02 +0900

    delete

--- Study0506/src/com/study/main/MemoParsing.java
+++ Study0506/src/com/study/main/MemoParsing.java
@@ -1,10 +1,31 @@
 package com.study.main;
 
 public class MemoParsing implements Chart{
-
+	String[][] table;  // chart 안에 들어있는 좌표 별 문자열 데이터
+	int curRow; // x
+	int curCol; // y
+	
 	@Override
 	public void insertCommand(String s) {
 		
 	}
+	@Override
+	public void delete() {
+		table[curRow][curCol] = null;
+	}
+	@Override
+	public void input(String s) {
+		
+	}
+	@Override
+	public void move(String s) {
+		
+	}
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	
 }
