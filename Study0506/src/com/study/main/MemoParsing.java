d5dbf9748686605743ca13947ac34cd497af75f2
miju <miju@192.168.0.8>
2023-05-06 17:20:20 +0900

    print

--- Study0506/src/com/study/main/MemoParsing.java
+++ Study0506/src/com/study/main/MemoParsing.java
@@ -1,9 +1,40 @@
 package com.study.main;
 
 public class MemoParsing implements Chart{
+	String[][] table;
+	int curRow;
+	int curCol;
 
 	@Override
 	public void insertCommand(String s) {
+		
+	}
+	
+	
+	@Override
+	public void print() {
+		if (table[curRow][curCol] == null) {
+			System.out.println("EMPTY");
+		} else {
+			System.out.println(table[curRow][curCol]);
+		}
+	}
+
+	@Override
+	public void move(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void input(String s) {
+		// TODO Auto-generated method stub
 		
 	}
 	
