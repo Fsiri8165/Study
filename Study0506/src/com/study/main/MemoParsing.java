c2534c25c6749bc4c305fd616f1ec14bdd654bca
EricNaKor <122855875+EricNakor@users.noreply.github.com>
2023-05-06 17:23:05 +0900

    input code

--- Study0506/src/com/study/main/MemoParsing.java
+++ Study0506/src/com/study/main/MemoParsing.java
@@ -1,10 +1,34 @@
 package com.study.main;
 
-public class MemoParsing implements Chart{
+public class MemoParsing implements Chart {
+	 private String[][] grid = new String[50][50];
+	 private int curRow = 0;
+	 private int curCol = 0;
+	    
+	@Override
+    public void insertCommand(String s) {
+    }
 
 	@Override
-	public void insertCommand(String s) {
-		
+	public void move(String s) {
+		// TODO Auto-generated method stub
+
 	}
-	
-}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+	 @Override
+	 public void input(String s) {
+	     grid[curRow][curCol] = s.replace("'", "");
+	    }
+	}
